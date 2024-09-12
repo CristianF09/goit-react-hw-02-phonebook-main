6858773eb76c356298929c6a11dd128bc867a1c0
CristianF09 <cris7i_laurentiu@yahoo.com>
2024-09-12 13:37:45 +0300

    Update ContactList.jsx

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import ContactListItem from './ContactListItem'; 
-import styles from './ContactList.module.css'; 
+import styles from './ContactList.module.css';
 
-const ContactList = ({ contacts, onDelete }) => {
-  return (
-    <ul className={styles.contactList}>
-      {contacts.map(contact => (
-        <ContactListItem
-          key={contact.id}
-          contact={contact}
-          onDelete={onDelete}
-        />
-      ))}
-    </ul>
-  );
-};
+const ContactList = ({ contacts, onDelete }) => (
+  <ul className={styles.contactList}>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id} className={styles.contactItem}>
+        <div className={styles.contactDetails}>
+          <p>{name}: {number}</p>
+        </div>
+        <button 
+          className={styles.deleteButton} 
+          onClick={() => onDelete(id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
 export default ContactList;
